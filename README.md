ac5c9ef5d7b0e43aa7811b5074135c1f6d0fd631
Killerjet101 <Stevenson0610@gmail.com>
2024-01-05 12:20:04 -0800

    Plugin development

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Replugged plugin template
+# OpenInApp
+Wanna have links go straight to the appropriate app without the browser opening? Use OpenInApp!!
 
-See the [plugin development guide](https://guide.replugged.dev/docs/plugins/getting-started) for
-more information on using this template.
+[![Install in Replugged](https://img.shields.io/badge/-Install%20in%20Replugged-blue?style=for-the-badge&logo=none)](https://replugged.dev/install?identifier=Killerjet101/replugged-OpenInApp&source=github)
 
-# License
-
-The [plugin template](https://github.com/replugged-org/plugin-template) is licensed under the
-[MIT](https://opensource.org/license/mit/) license. However, addons using this template are not
-necessarily licensed under MIT. Developers are encouraged to add a license file to their repository,
-but if none exists, the repository should be considered as unlicensed.
+## Supported Apps
+- [x] Steam
+- [x] Spotify
+- [x] Tidal
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
 import { Injector, Logger, webpack } from "replugged";
 
 const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const logger = Logger.plugin("OpenInApp");
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+  function clickOrigin(e: MouseEvent): { tagType: string; clickedUrl: string } {
+    const target = e.target as HTMLElement;
+    const tagType = target.tagName.toLowerCase();
+    const anchorElement = target.closest('a');
+    const clickedUrl = anchorElement ? anchorElement.href : "";
+    return { tagType, clickedUrl };
   }
+
+  const urls = {
+    steam: ["steamcommunity.com", "help.steampowered.com", "store.steampowered.com", "s.team"],
+    spotify: ["open.spotify.com", "play.spotify.com"],
+    tidal: ["tidal.com", "listen.tidal.com"],
+  };
+  const protocols: Record<string, string> = {
+    steam: "steam://openurl/",
+    spotify: "spotify://",
+    tidal: "tidal://",
+  };
+  const tagsToIdentify =  ["span", "img", "a"];
+
+  document.body.onclick = function (e: MouseEvent): void {
+    const origin = clickOrigin(e);
+    
+    for (let i = 0; i < tagsToIdentify.length; i++) {
+      if (origin.tagType === tagsToIdentify[i]) {
+        logger.log(`Tag type: ${origin.tagType}, Clicked URL: ${origin.clickedUrl}`);
+        const matchedUrl = Object.entries(urls).find(([_, urls]) =>
+          urls.some((url) => origin.clickedUrl.includes(url)),
+        );
+
+        if (matchedUrl) {
+          const appProtocol = protocols[matchedUrl[0]];
+          const appUrl = `${appProtocol}${origin.clickedUrl}`;
+          logger.log(`New URL: ${appUrl}`);
+          window.open(appUrl, "_blank", "noopener noreferrer");
+          e.preventDefault();
+        }
+      }
+    }
+  };
 }
 
 export function stop(): void {
